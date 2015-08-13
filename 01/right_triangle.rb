107f15a015ef7a21d2013b139dc5df8d0adda7a9
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-13 22:15:20 +0600

    fix right triangle

--- 01/right_triangle.rb
+++ 01/right_triangle.rb
@@ -1,13 +1,11 @@
-def max_of_three(a, b, c)
-  [a, b, c].max
+def isosceles_triangle?(a, b, c)
+  (a == b) && (a == c)
 end
 
 def right_triangle?(a, b, c)
-  a**2 + b**2 == c**2
-end
-
-def isosceles_triangle?(a, b, c)
-  (a == b) || (a == c) || (b == c)
+  return false if isosceles_triangle?(a, b, c)
+  sides = [a, b, c].sort
+  sides[0]**2 + sides[1]**2 == sides[2]**2
 end
 
 puts "Please enter the first side of triangle (in cm):"
@@ -19,20 +17,9 @@ second_side = gets.chomp.to_f
 puts "Please enter the third side of triangle (in cm):"
 third_side = gets.chomp.to_f
 
-max_side = max_of_three(first_side, second_side, third_side)
 
-if (first_side == max_side) && right_triangle?(second_side, third_side, first_side)
-  puts "The triangle is right."
-elsif (second_side == max_side) && right_triangle?(first_side, third_side, second_side)
-  puts "The triangle is right."
-elsif (third_side == max_side) && right_triangle?(first_side, second_side, third_side)
+if right_triangle?(second_side, third_side, first_side)
   puts "The triangle is right."
 else
   puts "The triangle is not right."
-end
-
-if isosceles_triangle?(first_side, second_side, third_side)
-  puts "The triangle is isosceles."
-else
-  puts "The triangle is not isosceles."
 end
