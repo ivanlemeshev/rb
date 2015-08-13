f4e9c8f150811c855432987af0db516430f04b63
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-13 18:14:59 +0600

    area of a triangle

---
+++ 01/area_of_a_triangle.rb
@@ -0,0 +1,12 @@
+def area_of_a_triangle(base, height)
+  1 / 2.0 * base * height
+end
+
+puts "Please enter the base of a triangle (in cm): "
+base = gets.chomp.to_f
+
+puts "Please enter the height of a triangle (in cm): "
+height = gets.chomp.to_f
+
+area = area_of_a_triangle(base, height)
+puts "The area of a triangle is #{area} cm."
