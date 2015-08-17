d7b2fadf5f18897d7f3e2dfa60accb004227936c
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-17 14:50:24 +0600

    months hash

---
+++ 02/months.rb
@@ -0,0 +1,16 @@
+months = {
+  January: 31,
+  February: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+}
+
+months.each { |month, days_count| puts month if days_count == 30 }
