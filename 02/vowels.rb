3297887d896a7fe342d38d24cc34956d599d2e8a
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-18 21:38:08 +0600

    fix vowesl

--- 02/vowels.rb
+++ 02/vowels.rb
@@ -1,5 +1,6 @@
 alphabet = ('a'..'z').to_a
-volwes = ['a', 'e', 'i', 'o', 'u', 'y'].inject({}) do |result, letter|
-  result.merge!({ letter => alphabet.index(letter) + 1 })
+volwes = {}
+['a', 'e', 'i', 'o', 'u', 'y'].each do |letter|
+  volwes[letter] = alphabet.index(letter) + 1 if alphabet.include?(letter)
 end
 puts volwes
