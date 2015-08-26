96f7b69dd51af11035cd097051af1d5245fd79d1
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-26 19:02:04 +0600

    add PassangerTrain

---
+++ 04/passanger_train.rb
@@ -0,0 +1,28 @@
+class PassangerTrain < Train
+  def initialize
+    super
+    @type = Train::TYPE_PASSANGER
+  end
+
+  def attach_wagon(wagon)
+    if passanger_wagon?(wagon)
+      super
+    else
+      puts "The wagon can not be attached. The wagon is not #{wagon.type}."
+    end
+  end
+
+  def detach_wagon(wagon)
+    if passanger_wagon?(wagon)
+      super
+    else
+      puts "The wagon can not be detached. The wagon is not #{wagon.type}."
+    end
+  end
+
+  private
+
+  def passanger_wagon?(wagon)
+    wagon.type == Wagon::TYPE_PASSANGER
+  end
+end
