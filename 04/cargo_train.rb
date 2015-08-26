46370b301b70ae4924b2387e4e86680c6d55e818
Ivan Lemeshev <vanlmshv@gmail.com>
2015-08-26 19:01:55 +0600

    add CargoTrain

---
+++ 04/cargo_train.rb
@@ -0,0 +1,28 @@
+class CargoTrain < Train
+  def initialize
+    super
+    @type = Train::TYPE_CARGO
+  end
+
+  def attach_wagon(wagon)
+    if cargo_wagon?(wagon)
+      super
+    else
+      puts "The wagon can not be attached. The wagon is not #{wagon.type}."
+    end
+  end
+
+  def detach_wagon(wagon)
+    if cargo_wagon?(wagon)
+      super
+    else
+      puts "The wagon can not be detached. The wagon is not #{wagon.type}."
+    end
+  end
+
+  private
+
+  def cargo_wagon?(wagon)
+    wagon.type == Wagon::TYPE_CARGO
+  end
+end
