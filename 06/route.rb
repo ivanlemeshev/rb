8031a99f68903dc556e92a79e7bc402b261c9c66
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-06 04:36:05 +0600

    validate method params

--- 06/route.rb
+++ 06/route.rb
@@ -5,33 +5,49 @@ class Route
     @stations = []
   end
 
+  def add_station(station)
+    validate_station!(station)
+    self.stations << station
+  end
+
+  def delete_station(station)
+    validate_station!(station)
+    self.stations.delete(station)
+  end
+
   def show_stations
     if self.stations.empty?
-      puts "There are no stations in the route."
+      puts 'There are no stations in the route.'
     else
       self.stations.each { |station| puts station.name }
     end
   end
 
-  def add_station(station)
-    self.stations << station
-    puts "The station #{station.name} is added to the route."
-  end
-
-  def delete_station(station)
-    self.stations.delete(station)
-    puts "The station #{station.name} is deleted from the route."
-  end
-
   def previous_station(station)
-    previous_index = self.stations.index(station) - 1
-    return self.stations[previous_index] if previous_index >= 0
-    nil
+    station_index(station) do |index|
+      previous_index = index - 1
+      return self.stations[previous_index] if previous_index >= 0
+    end
   end
 
   def next_station(station)
-    next_index = self.stations.index(station) + 1
-    return self.stations[next_index] if next_index < self.stations.size
+    station_index(station) do |index|
+      next_index = index + 1
+      return self.stations[next_index] if next_index < self.stations.size
+    end
+  end
+
+  private
+
+  def validate_station!(station)
+    raise 'ivalid station' unless station.is_a? RailwayStation
+  end
+
+  def station_index(station)
+    validate_station!(station)
+    station_index = self.stations.index(station)
+    return nil unless station_index
+    yield station_index
     nil
   end
 end
